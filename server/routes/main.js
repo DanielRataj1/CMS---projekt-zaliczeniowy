3fc1f48ec07055465c309b8cb9d4ab6f44f3b582
Mehirion <Mehirion1@gmail.com>
2024-04-04 22:14:40 +0200

    added css

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,12 +1,28 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('', (req, res)=> {
-    const locals = {
-        title: "CMS Projekt Blog",
-        description:"Blog NodeJS/Express/MongoDB"
-    }
-    res.render('index', {locals})
-})
+router.get("", (req, res) => {
+  const locals = {
+    title: "CMS Projekt Blog",
+    description: "Blog NodeJS/Express/MongoDB",
+  };
+  res.render("index", { locals });
+});
 
-module.exports = router
+router.get("/about", (req, res) => {
+  const locals = {
+    title: "CMS Projekt Blog",
+    description: "Blog NodeJS/Express/MongoDB",
+  };
+  res.render("about", { locals });
+});
+
+router.get("/contact", (req, res) => {
+  const locals = {
+    title: "CMS Projekt Blog",
+    description: "Blog NodeJS/Express/MongoDB",
+  };
+  res.render("contact", { locals });
+});
+
+module.exports = router;
