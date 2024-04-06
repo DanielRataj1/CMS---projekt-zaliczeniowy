4e2c90d47445c415a3f7d9cbbbb3866f7dfd81e9
Mehirion <Mehirion1@gmail.com>
2024-04-06 15:51:31 +0200

    DB added

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const expressLayout = require('express-ejs-layouts')
 
+const connectDB = require('./server/config/db')
+
 const app = express()
 const PORT = 5000 || process.env.PORT
+
+// łączenie z bazą danych
+
+connectDB()
 
 app.use(express.static('public'))
 
@@ -18,5 +24,5 @@ app.set('view engine', 'ejs')
 app.use('/', require('./server/routes/main'))
 
 app.listen(PORT, ()=> {
-    console.log(`App listening on port: ${PORT}`)
+    console.log(`App listening on port: ` + `http://localhost:${PORT}`)
 })
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Post = require('../models/Post')
 
-router.get("", (req, res) => {
+router.get("", async (req, res) => {
   const locals = {
     title: "CMS Projekt Blog",
     description: "Blog NodeJS/Express/MongoDB",
   };
-  res.render("index", { locals });
+
+try {
+  const data = await Post.find()
+  res.render('index', {locals, data})
+} catch (error) {
+  console.log(error)
+}
 });
+
+
 
 router.get("/about", (req, res) => {
   const locals = {
@@ -26,3 +35,16 @@ router.get("/contact", (req, res) => {
 });
 
 module.exports = router;
+
+
+//test bazy danych
+// function insertPostData (){
+//   Post.insertMany([
+//     {
+//       title:"Tworzenie bloga",
+//       body:"Treść wpisz tutaj"
+//     },
+//   ])
+// }
+
+// insertPostData()
