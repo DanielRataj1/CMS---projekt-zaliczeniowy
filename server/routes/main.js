3fe08aa54a3dfe1ef2f3ec40093399c9e0c95f77
Mehirion <Mehirion1@gmail.com>
2024-04-10 18:44:59 +0200

    Update, pages

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,20 +1,65 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 const Post = require('../models/Post')
 
-router.get("", async (req, res) => {
+router.get('', async (req, res) => {
+try {
   const locals = {
     title: "CMS Projekt Blog",
     description: "Blog NodeJS/Express/MongoDB",
-  };
+  }
 
-try {
-  const data = await Post.find()
+
+  let perPage = 5;
+  let page = req.query.page || 1
+
+  const data = await Post.aggregate([ { $sort: {createdAt: -1 } } ])
+  .skip(perPage * page - perPage)
+  .limit(perPage)
+  .exec()
+
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render('index', { 
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+      currentRoute: '/'
+    });
+
+
+
   res.render('index', {locals, data})
 } catch (error) {
   console.log(error)
 }
 });
+
+
+
+
+
+
+// backup
+// router.get('', async (req, res) => {
+//   const locals = {
+//     title: "CMS Projekt Blog",
+//     description: "Blog NodeJS/Express/MongoDB",
+//   };
+
+// try {
+//   const data = await Post.find()
+//   res.render('index', {locals, data})
+// } catch (error) {
+//   console.log(error)
+// }
+// });
+
+
+
 
 
 
