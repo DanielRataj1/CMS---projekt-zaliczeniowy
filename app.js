9f6e69a0d55c474286add9d6c1a77ef7ec0101d3
Mehirion <Mehirion1@gmail.com>
2024-04-14 21:51:46 +0200

    Admin panel is alive

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 
 const express = require('express')
 const expressLayout = require('express-ejs-layouts')
+const cookieParser = require('cookie-parser')
+const MongoStore = require('connect-mongo')
+const session = require('express-session')
 
 const connectDB = require('./server/config/db')
 
@@ -11,6 +14,20 @@ const PORT = 5000 || process.env.PORT
 // łączenie z bazą danych
 
 connectDB()
+
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+app.use(cookieParser())
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URL
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+  }))
 
 app.use(express.static('public'))
 
